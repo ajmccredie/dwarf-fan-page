b0038689d0e13256e7f28485ab3affa1f66371a8
ajmccredie <>
2023-10-05 16:52:43 +0000

    Add the import os to the run file

---
+++ run.py
@@ -0,0 +1,14 @@
+import os
+from flash import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello,</h1> <h2>World</h2>"
+
+if __name__== "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
