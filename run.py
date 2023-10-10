20c9039292e019542144f4c2869c424336c24019
ajmccredie <>
2023-10-10 12:45:56 +0000

    Add url_for to the menu items on both pages

--- run.py
+++ run.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello,</h1> <h2>World</h2>"
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
