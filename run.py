05eab31789b47a64719215ad2354bcb85791ad33
ajmccredie <>
2023-10-10 12:36:00 +0000

    Add home and about html pages and debug run file

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 import os
-from flash import Flask
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "<h1>Hello,</h1> <h2>World</h2>"
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
