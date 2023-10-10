b152a3b0254787d03c7653d7f9d162a3ea232482
ajmccredie <>
2023-10-10 13:03:25 +0000

    Add contact page nav elements and app routes

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,9 +8,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
